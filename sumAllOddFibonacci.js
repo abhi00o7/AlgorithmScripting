5f74e66aee2f924f23b7d288974ef74852de1de8
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-06-06 22:14:24 +0530

    Create sumAllOddFibonacci.js
    Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.

---
+++ sumAllOddFibonacci.js
@@ -0,0 +1,21 @@
+// Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
+
+function sumFibs(num) {
+    let previous = 0;
+    let current = 1;
+    let sum = 0;
+    let next;
+
+    for (current; current <= num;) {
+        next = current + previous
+        previous = current
+        //   check for odd number
+        if (current % 2 !== 0) {
+            sum += current
+        }
+        current = next
+    }
+    return sum
+}
+
+console.log(sumFibs(54816843183848))
