bcf807b4e57d059798c74bd79ba2e713dfb12db3
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-06-09 14:08:48 +0530

    Update steamroller.js
    added test cases

--- steamroller.js
+++ steamroller.js
@@ -11,3 +11,17 @@ function steamrollArray(arr) {
 console.log(steamrollArray([1, {},[3, [[4]]]]))
 
 
+// test cases
+// steamrollArray([[["a"]], [["b"]]]) should return ["a", "b"].
+
+// Passed
+// steamrollArray([1, [2], [3, [[4]]]]) should return [1, 2, 3, 4].
+
+// Passed
+// steamrollArray([1, [], [3, [[4]]]]) should return [1, 3, 4].
+
+// Passed
+// steamrollArray([1, {}, [3, [[4]]]]) should return [1, {}, 3, 4].
+
+// Passed
+// Your solution should not use the Array.prototype.flat() or Array.prototype.flatMap() methods.
