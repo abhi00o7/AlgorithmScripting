24a2ee52757053595926e2126c9774b06f2f1c01
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-06-09 16:56:01 +0530

    Update everythingBeTrue.js
    added test cases.

--- everythingBeTrue.js
+++ everythingBeTrue.js
@@ -29,3 +29,113 @@ console.log(truthCheck([{
     "user": "Po",
     "sex": "female"
 }], "sex"))
+
+// test cases
+
+
+truthCheck([{
+    "user": "Tinky-Winky",
+    "sex": "male"
+}, {
+    "user": "Dipsy",
+    "sex": "male"
+}, {
+    "user": "Laa-Laa",
+    "sex": "female"
+}, {
+    "user": "Po",
+    "sex": "female"
+}], "sex") 
+// should return true.
+
+// // Passed
+truthCheck([{
+    "user": "Tinky-Winky",
+    "sex": "male"
+}, {
+    "user": "Dipsy"
+}, {
+    "user": "Laa-Laa",
+    "sex": "female"
+}, {
+    "user": "Po",
+    "sex": "female"
+}], "sex")
+//  should return false.
+
+// // Passed
+truthCheck([{
+    "user": "Tinky-Winky",
+    "sex": "male",
+    "age": 0
+}, {
+    "user": "Dipsy",
+    "sex": "male",
+    "age": 3
+}, {
+    "user": "Laa-Laa",
+    "sex": "female",
+    "age": 5
+}, {
+    "user": "Po",
+    "sex": "female",
+    "age": 4
+}], "age") 
+// should return false.
+
+// // Passed
+truthCheck([{
+    "name": "Pete",
+    "onBoat": true
+}, {
+    "name": "Repeat",
+    "onBoat": true
+}, {
+    "name": "FastForward",
+    "onBoat": null
+}], "onBoat") 
+// should return false.
+
+// Passed
+truthCheck([{
+    "name": "Pete",
+    "onBoat": true
+}, {
+    "name": "Repeat",
+    "onBoat": true,
+    "alias": "Repete"
+}, {
+    "name": "FastForward",
+    "onBoat": true
+}], "onBoat") 
+// should return true.
+
+// Passed
+truthCheck([{
+    "single": "yes"
+}], "single")
+//  should return true.
+
+// Passed
+truthCheck([{
+    "single": ""
+}, {
+    "single": "double"
+}], "single")
+//  should return false.
+
+// Passed
+truthCheck([{
+    "single": "double"
+}, {
+    "single": undefined
+}], "single") 
+// should return false.
+
+// Passed
+truthCheck([{
+    "single": "double"
+}, {
+    "single": NaN
+}], "single") 
+// should return false.
