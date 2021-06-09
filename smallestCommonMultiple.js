9568b1280ae019f270e6421eed24625bd334e5bd
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-06-09 10:56:04 +0530

    Update smallestCommonMultiple.js
    changed indentation.

--- smallestCommonMultiple.js
+++ smallestCommonMultiple.js
@@ -4,20 +4,24 @@
 
 // For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly divisible by all numbers between 1 and 3. The answer here would be 6.
 function smallestCommons(arr) {
-    // Setup
-    const [min, max] = arr.sort((a, b) => a - b);
-    const range = Array(max - min + 1)
-      .fill(0)
-      .map((_, i) => i + min);
-    // GCD of two numbers
-    // https://en.wikipedia.org/wiki/Greatest_common_divisor#Euclid's_algorithm
-    const gcd = (a, b) => (b === 0) ? a : gcd(b, a % b);
-    // LCM of two numbers
-    // https://en.wikipedia.org/wiki/Least_common_multiple#Using_the_greatest_common_divisor
-    const lcm = (a, b) => a * b / gcd(a, b);
-    // LCM of multiple numbers
-    // https://en.wikipedia.org/wiki/Least_common_multiple#Other
-    return range.reduce((multiple, curr) => lcm(multiple, curr));
-  }
-  
-  smallestCommons([1, 5]);
+  // Setup
+  const [min, max] = arr.sort((a, b) => a - b);
+
+  const range = Array(max - min + 1)
+    .fill(0)
+    .map((_, i) => i + min);
+
+  // GCD of two numbers
+  // https://en.wikipedia.org/wiki/Greatest_common_divisor#Euclid's_algorithm
+  const gcd = (a, b) => (b === 0) ? a : gcd(b, a % b);
+
+  // LCM of two numbers
+  // https://en.wikipedia.org/wiki/Least_common_multiple#Using_the_greatest_common_divisor
+  const lcm = (a, b) => a * b / gcd(a, b);
+
+  // LCM of multiple numbers
+  // https://en.wikipedia.org/wiki/Least_common_multiple#Other
+  return range.reduce((multiple, curr) => lcm(multiple, curr));
+}
+
+smallestCommons([1, 5]);
