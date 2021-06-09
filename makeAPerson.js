65a140ed2d56cdd451985e31df0e72fc12ec3bbe
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-06-09 22:11:16 +0530

    Update makeAPerson.js
    added test cases

--- makeAPerson.js
+++ makeAPerson.js
@@ -38,3 +38,41 @@ var bob = new Person('Bob Ross');
 
 bob.setLastName("Curry")
 console.log(bob.getFullName())
+
+
+/*
+* test cases
+* Object.keys(bob).length should return 6.
+
+* Passed
+* bob instanceof Person should return true.
+
+* Passed
+* bob.firstName should return undefined.
+
+* Passed
+* bob.lastName should return undefined.
+
+* Passed
+* bob.getFirstName() should return the string Bob.
+
+* Passed
+* bob.getLastName() should return the string Ross.
+
+* Passed
+* bob.getFullName() should return the string Bob Ross.
+
+* Passed
+* bob.getFullName() should return the string Haskell Ross after bob.setFirstName("Haskell").
+
+* Passed
+* bob.getFullName() should return the string Haskell Curry after bob.setLastName("Curry").
+
+* Passed
+* bob.getFullName() should return the string Haskell Curry after bob.setFullName("Haskell Curry").
+
+* Passed
+* bob.getFirstName() should return the string Haskell after bob.setFullName("Haskell Curry").
+
+* Passed
+* bob.getLastName() should return the string Curry after bob.setFullName("Haskell Curry"). */
