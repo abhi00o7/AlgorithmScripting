02c82cf652a6b3aa9944362203020b1b79fe4565
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-10-26 23:03:01 +0530

    Create title-case-a-sentence.js

---
+++ title-case-a-sentence.js
@@ -0,0 +1,11 @@
+function titleCase(str) {
+  var strArray = str.toLowerCase().split(" ")
+
+  for (let index = 0; index < strArray.length; index++){
+    strArray[index] = strArray[index].replace((strArray[index].slice(0,1)),(strArray[index].slice(0,1).toUpperCase()))
+  }
+  console.log(strArray.join(" "))
+  return strArray.join(" ");
+}
+
+titleCase("I'm a little tea pot");
