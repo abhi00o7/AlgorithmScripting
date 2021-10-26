2acca0d5973c53e36a27e19ecccc0f585cc933a4
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-10-26 23:03:44 +0530

    Create argumentsOptional.js

---
+++ argumentsOptional.js
@@ -0,0 +1,5 @@
+// A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). In other words, a closure gives you access to an outer function’s scope from an inner function. In JavaScript, closures are created every time a function is created, at function creation time.
+
+// Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
+
+// For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
