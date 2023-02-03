2cbd8fea31833c547da6a2fa9efea823d5d1c38a
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2023-02-03 23:35:03 +0530

    add max sum K corner

---
+++ maxSumKcorner.mjs
@@ -0,0 +1,8 @@
+/**
+ * Given an array arr[] and an integer K, the task is to 
+ * find the maximize the sum of K elements in the Array by taking only 
+ * corner elements.
+ * A corner element is an element from the start of the array or 
+ * from the end of the array.
+ */
+
