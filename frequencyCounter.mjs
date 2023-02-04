956bb10111b0a71d9b188c10182bf2aea19cd122
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2023-02-04 23:42:27 +0530

    add frequency counter

---
+++ frequencyCounter.mjs
@@ -0,0 +1,11 @@
+/**
+ * Write a function called same, which accepts two arrays.
+ The function should return true
+ if every value in the array has it 's corresponding value squared in the second array.
+ The frequency of values must be the same.
+ 
+ */
+
+function checkSame(arr1, arr2){
+  
+}
