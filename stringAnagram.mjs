2cd162a53c25d0dac258580aaa139f1b643ffd8c
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2023-02-05 02:36:18 +0530

    add stringAnagram

---
+++ stringAnagram.mjs
@@ -0,0 +1,32 @@
+/**
+ * Given two strings, write a function to determine
+ if the second string is an anagram of the first.An anagram is
+ a word, phrase, or name formed by rearranging the
+ letters of another, such as cinema, formed from iceman.
+ */
+
+const frequency = (countObj, element) => {
+  const currentCount = countObj[element] ?? 0
+  return {
+    ...countObj,
+    [element]: currentCount + 1
+  }
+}
+ 
+function isAnagram(str1, str2){
+  // check the length 
+  if(str1.length !== str2.length) return false
+
+  // frequency of letters in string
+  const frequencyStr1 = str1.split('').reduce(frequency, 0);
+  const frequencyStr2 = str2.split('').reduce(frequency, 0);
+
+  for (const letter in frequencyStr2) {
+    if (!frequencyStr1[letter]) return false;
+    if (frequencyStr1[letter] !== frequencyStr2[letter]) return false;
+  }
+  return true
+}
+
+console.log(isAnagram(`cinema`, `iceman`))
+console.log(isAnagram(`azz`, `zza`))
