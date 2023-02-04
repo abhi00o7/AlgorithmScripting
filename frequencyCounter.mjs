343bed153c008410add7ede46a54c0167126d3e8
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2023-02-05 02:36:00 +0530

    solution freq counter

--- frequencyCounter.mjs
+++ frequencyCounter.mjs
@@ -6,6 +6,28 @@
  
  */
 
-function checkSame(arr1, arr2){
-  
+
+const frequency = (countObj, element) => {
+  const currentCount = countObj[element] ?? 0;
+  return {
+    ...countObj,
+    [element]: currentCount + 1
+  }
 }
+
+function checkSame(arr1, arr2) {
+  // check if the length is same 
+  if (arr1.length !== arr2.length) return false;
+
+  // get frequency of each element of the arrays
+  const frequencyArr1 = arr1.reduce(frequency, 0);
+  const frequencyArr2 = arr2.reduce(frequency, 0);
+  
+  for (const num in frequencyArr1) {
+    if (!frequencyArr2[num ** 2]) return false 
+    if (frequencyArr1[num] !== frequencyArr2[num ** 2]) return false
+  }
+  return true
+}
+
+console.log(checkSame([1, 2, 2, 4, 2, 3], [4, 16, 4, 1, 4, 10]))
