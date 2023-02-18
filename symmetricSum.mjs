a74f7a1fea1cc1ea5cd904205e47f854d9de735a
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2023-02-18 18:07:33 +0530

    update two Array intersection

--- symmetricSum.mjs
+++ symmetricSum.mjs
@@ -5,33 +5,26 @@
  */
 
 
-const frequency = (countObj, element) => {
-  const currentCount = countObj[element] ?? 0
-  return {
-    ...countObj,
-    [element]: currentCount + 1
+// the following algorithm works perfectly well for upto two arrays.
+
+function sym(...args) {
+  let oneArray = []
+  for (const arg of args) {
+    oneArray = oneArray.concat([...new Set(arg)])
   }
+
+  return oneArray.filter((element, index, array) => {
+    return  array.indexOf(element) === array.lastIndexOf(element)
+  });
 }
 
-
-function sym(arr1, arr2) {
-  // check the length 
-  // if (arr1.length !== arr2.length) return false
-
-  // frequency of letters in string
-  const frequencyArr1 = arr1.split('').reduce(frequency, 0);
-  console.log("🚀 ~ file: maxSumKcorner.mjs:24 ~ sym ~ frequencyArr1", frequencyArr1)
-  // const arrayArr2 = arr2.split('')
-
-  for (const number of arr2) {
-    if (!frequencyArr1[number]) return false;
-    frequencyArr1[number] -= 1
-  }
-  return true
-}
 
 const t0 = performance.now();
-sym([1, 2, 3], [5, 2, 1, 4]);
-console.log("🚀 ~ file: maxSumKcorner.mjs:35 ~ sym([1, 2, 3], [5, 2, 1, 4])", sym([1, 2, 3], [5, 2, 1, 4]))
+console.log(sym([1, 2, 3], [5, 2, 1, 4]))
 const t1 = performance.now();
+console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
+
+const t2 = performance.now();
+console.log(sym([1, 2, 3, 3], [5, 2, 1, 4]))
+const t3 = performance.now();
 console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
