3b5eb9d1cb2ec3eb3a9527c638e6eecfe00e244b
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2023-03-09 23:58:19 +0530

    add hign order function timer

--- symmetricDiff.mjs
+++ symmetricDiff.mjs
@@ -34,29 +34,67 @@ const diff = (arr1, arr2) => [
 const anotherSym = (...args) => [...new Set(args.reduce(diff, []))];
 
 // test here
-sym([1, 2, 3], [5, 2, 1, 4]);
+// sym([1, 2, 3], [5, 2, 1, 4]);
 
 // test cases
-console.log(sym([1, 2, 5], [2, 3, 5], [3, 4, 5]))
-console.log(sym([1, 2, 3, 3], [5, 2, 1, 4]))
-console.log(sym([1, 2, 5], [2, 3, 5], ))
-console.log(sym([1, 2, 5]))
-console.log(sym([1, 3], [3, 4, 5]))
-console.log(sym())
+// console.log(sym([1, 2, 5], [2, 3, 5], [3, 4, 5]))
+// console.log(sym([1, 2, 3, 3], [5, 2, 1, 4]))
+// console.log(sym([1, 2, 5], [2, 3, 5], ))
+// console.log(sym([1, 2, 5]))
+// console.log(sym([1, 3], [3, 4, 5]))
+// console.log(sym())
 
 // performance test cases
 
-const t0 = performance.now();
-console.log(sym([1, 2, 5], [2, 3, 5], [3, 4, 5]))
-const t1 = performance.now();
-console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
+// higherOrder functions
+// higher order function
+function performanceTimer(func) {
+  return function (...args) {
+    const t0 = performance.now();
+    const results = func.apply(null, args)
+    const t1 = performance.now();
+    console.log(`Function Call took ${t1 - t0} milliseconds.`);
+    return results
+  }
+}
 
-const t2 = performance.now();
-console.log(sym([1, 2, 3, 3], [5, 2, 1, 4]))
-const t3 = performance.now();
-console.log(`Call to doSomething took ${t3 - t2} milliseconds.`);
+const firstFuncTimer = performanceTimer(sym)
+const secondFuncTimer = performanceTimer(diff)
+const thirdFuncTimer = performanceTimer(anotherSym)
 
-const t4 = performance.now();
-console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]))
-const t5 = performance.now();
-console.log(`Call to doSomething took ${t5 - t4} milliseconds.`);
+console.log(firstFuncTimer([1, 2, 5], [2, 3, 5], [3, 4, 5]))
+console.log(firstFuncTimer([1, 2, 3, 3], [5, 2, 1, 4]))
+console.log(firstFuncTimer([1, 2, 5], [2, 3, 5], ))
+console.log(firstFuncTimer([1, 2, 5]))
+console.log(firstFuncTimer([1, 3], [3, 4, 5]))
+console.log(firstFuncTimer())
+
+console.log(secondFuncTimer([1, 2, 5], [2, 3, 5], [3, 4, 5]))
+console.log(secondFuncTimer([1, 2, 3, 3], [5, 2, 1, 4]))
+// console.log(secondFuncTimer([1, 2, 5], [2, 3, 5], ))
+console.log(secondFuncTimer([1, 2, 5]))
+console.log(secondFuncTimer([1, 3], [3, 4, 5]))
+// console.log(secondFuncTimer())
+
+console.log(thirdFuncTimer([1, 2, 5], [2, 3, 5], [3, 4, 5]))
+console.log(thirdFuncTimer([1, 2, 3, 3], [5, 2, 1, 4]))
+console.log(thirdFuncTimer([1, 2, 5], [2, 3, 5], ))
+console.log(thirdFuncTimer([1, 2, 5]))
+console.log(thirdFuncTimer([1, 3], [3, 4, 5]))
+// console.log(thirdFuncTimer())
+
+
+// const t0 = performance.now();
+// console.log(sym([1, 2, 5], [2, 3, 5], [3, 4, 5]))
+// const t1 = performance.now();
+// console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
+
+// const t2 = performance.now();
+// console.log(sym([1, 2, 3, 3], [5, 2, 1, 4]))
+// const t3 = performance.now();
+// console.log(`Call to doSomething took ${t3 - t2} milliseconds.`);
+
+// const t4 = performance.now();
+// console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]))
+// const t5 = performance.now();
+// console.log(`Call to doSomething took ${t5 - t4} milliseconds.`);
