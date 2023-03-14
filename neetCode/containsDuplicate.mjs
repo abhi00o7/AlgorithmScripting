6e4946de884c3f40f68da08b4233d69333f7328d
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2023-03-15 02:43:17 +0530

    add new solutions contains duplicate

--- neetCode/containsDuplicate.mjs
+++ neetCode/containsDuplicate.mjs
@@ -4,14 +4,20 @@
  */
 
 /**
- * @param {number[]} num
- * @return {boolean}
  */
-const containsDuplicate = function (numArr) {
-  return numArr.filter((num, index) =>
-    numArr.lastIndexOf(num) !== index ? true : false
-  );
+const containsDuplicate = function (numArr = []) {
+  return numArr.foreach((num, index) => {
+    if (numArr.lastIndexOf(num) !== index) return true;
+  });
 };
 
+function containsDuplicateV2(numArr = []) {
+  return numArr.some((element, index) => numArr.lastIndexOf(element) !== index);
+}
 
-console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); //true
+function containsDuplicateV3(numArr = []) {
+  return [...new Set(numArr)].length !== numArr.length;
+}
+console.log(containsDuplicateV3([1, 2, 3, 4, 5, 6])); //false
+console.log(containsDuplicateV3([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); //true
+console.log(containsDuplicateV2()); //true
