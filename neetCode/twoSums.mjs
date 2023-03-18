006b616f1116b1a44b5b878f4b3c060977162135
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2023-03-18 23:53:54 +0530

    add performance timer

--- neetCode/twoSums.mjs
+++ neetCode/twoSums.mjs
@@ -30,8 +30,24 @@ const twoSum = function (numArray, target) {
   return `target not possible`;
 };
 
-console.log(twoSumHashTable([2, 7, 11, 15], 9));
-console.log(twoSumHashTable([2, 7, 11, 15], 18));
-console.log(twoSumHashTable([3, 2, 4, 6], 8));
-console.log(twoSumHashTable([3, 2, 4], 6));
-console.log(twoSumHashTable([-1, -2, -3, -4, -5], -8));
+
+function performanceTimer(func) {
+  let count = 0;
+  return function (...args) {
+    const t0 = performance.now();
+    const results = func.apply(null, args);
+    const t1 = performance.now();
+    console.log(`Function Call took ${t1 - t0} milliseconds. COUNT:${++count}`);
+    return results;
+  };
+}
+
+const twoSumV1 = performanceTimer(twoSum)
+
+twoSumV1([2, 7, 11, 15], 9)
+
+// console.log(twoSumHashTable([2, 7, 11, 15], 9));
+// console.log(twoSumHashTable([2, 7, 11, 15], 18));
+// console.log(twoSumHashTable([3, 2, 4, 6], 8));
+// console.log(twoSumHashTable([3, 2, 4], 6));
+// console.log(twoSumHashTable([-1, -2, -3, -4, -5], -8));
