062480f6560ae89513bd89b327c7034497abb9eb
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2023-04-18 22:39:10 +0530

    add longest substring no repeat

---
+++ neetCode/longestNoRepeat.mjs
@@ -0,0 +1,18 @@
+// Length of Longest Substring without any Repeating Character
+
+const longestNoRepeat = (str) => {
+  let longest = 0;
+  let start = 0;
+  const map = new Map();
+  for (let i = 0; i < str.length; i++) {
+    if (map.has(str[i])) {
+      start = Math.max(start, map.get(str[i]) + 1);
+    }
+    map.set(str[i], i);
+    longest = Math.max(longest, i - start + 1);
+  }
+  return longest;
+}
+
+console.log(longestNoRepeat('abcabcbb')); // 3
+console.log(longestNoRepeat('bbbbb')); // 1
