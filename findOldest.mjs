ad6bcb2d17805cfc2c6338fc45a741dca25f18b8
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2023-05-04 12:53:10 +0530

    fix oldest

--- findOldest.mjs
+++ findOldest.mjs
@@ -1,23 +1,36 @@
 function findOldestPerson(arr) {
-  console.log(arr)
-  return Math.max(arr.map(obj => obj.age))
+  console.log(arr);
+  return Math.max(arr.map((obj) => obj.age));
 }
-
 
 // const findOldestPerson = (arr) => arr.filter((obj, index) => {
 //   let maxAge = obj[index] ?? 0
 //   if()
 //   })
 
+console.log(
+  findOldestPerson([
+    {
+      name: "Liam",
+      age: 28,
+    },
+    {
+      name: "Lisa",
+      age: 2,
+    },
+    {
+      name: "lucy",
+      age: 289,
+    },
+  ])
+);
 
-console.log(findOldestPerson([{
-  name: 'Liam',
-  age: 28
-}, {
-  name: 'Lisa',
-  age: 2
-}, {
-  name: 'lucy',
-  age: 289
-}, ]))
-
+// find the oldest person in an array of objects
+const findOldestPerson = (arr) =>
+  arr.filter((obj, index) => {
+    let maxAge = obj[index] ?? 0;
+    if (obj.age > maxAge) {
+      maxAge = obj.age;
+    }
+    return maxAge;
+  });
