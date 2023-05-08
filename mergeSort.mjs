541d73384a605beee95daabc42abb0fa6b1d2e05
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2023-05-08 23:58:06 +0530

    update mergeSort

--- mergeSort.mjs
+++ mergeSort.mjs
@@ -6,6 +6,26 @@ const mergeSort = (arr) => {
   const leftArr = arr.slice(0, middle);
   const rightArr = arr.slice(middle, arr.length);
   return merge(mergeSort(leftArr), mergeSort(rightArr));
-}
+};
 
 const merge = (leftArr, rightArr) => {
+  leftArr = leftArr || [];
+  rightArr = rightArr || [];
+  const result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+    if (leftArr[leftIndex] < rightArr[rightIndex]) {
+      result.push(leftArr[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(rightArr[rightIndex]);
+      rightIndex++;
+    }
+  }
+};
+
+
+// examples
+
+console.log(mergeSort([1, 5, 2, 4, 3, 6, 7, 8, 9, 10]))
