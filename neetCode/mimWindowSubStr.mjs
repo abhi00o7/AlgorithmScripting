3c9d231f341d1b3a0ab5628d37e575f20e244bdc
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2023-06-02 23:55:15 +0530

    create minimum window substr

---
+++ neetCode/mimWindowSubStr.mjs
@@ -0,0 +1,9 @@
+/**
+ * Given two strings s and t of lengths m and n respectively, return the minimum window 
+substring
+ of s such that every character in t (including duplicates) is included in the window. 
+ If there is no such substring, return the empty string "".
+
+The test cases will be generated such that the answer is unique.
+ */
+
