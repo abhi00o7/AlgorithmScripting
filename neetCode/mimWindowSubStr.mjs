004518bfdf5aa41079892d66aab303b0e5b14a7a
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2023-06-04 23:18:03 +0530

    update minWIndowSubstr

--- neetCode/mimWindowSubStr.mjs
+++ neetCode/mimWindowSubStr.mjs
@@ -7,3 +7,49 @@ substring
 The test cases will be generated such that the answer is unique.
  */
 
+
+function minWindow(s, t) {
+  let left = 0;
+  let right = 0;
+  let min = Infinity;
+  let minStr = '';
+  let map = {};
+
+  for (let char of t) {
+    map[char] = map[char] + 1 || 1;
+  }
+
+  let count = Object.keys(map).length;
+
+  while (right < s.length) {
+    let char = s[right];
+
+    if (map[char] !== undefined) {
+      map[char]--;
+      if (map[char] === 0) count--;
+    }
+
+    while (count === 0) {
+      let temp = s[left];
+
+      if (map[temp] !== undefined) {
+        map[temp]++;
+        if (map[temp] > 0) count++;
+      }
+
+      if (right - left + 1 < min) {
+        min = right - left + 1;
+        minStr = s.slice(left, right + 1);
+      }
+
+      left++;
+    }
+
+    right++;
+  }
+
+  return minStr;
+}
+
+// example 1
+console.log(minWindow("ADOBECODEBANC", "ABC")); // "BANC"
