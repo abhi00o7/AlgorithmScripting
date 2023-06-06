fbdd3fe6c0e3609c7e8fc34d3ea15ece68bb89ba
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2023-06-06 22:12:12 +0530

    alternate sol min window

--- neetCode/mimWindowSubStr.mjs
+++ neetCode/mimWindowSubStr.mjs
@@ -55,3 +55,93 @@ function minWindow(s, t) {
 console.log(minWindow("ADOBECODEBANC", "ABC")); // "BANC"
 // example 2
 console.log(minWindow("a", "a")); // "a"
+
+// another method
+
+function minWindow(s, t) {
+  let left = 0;
+  let right = 0;
+  let min = Infinity;
+  let minStr = '';
+  let map = {};
+
+  for (let char of t) {
+    map[char] = map[char] + 1 || 1;
+  }
+
+  let count = Object.keys(map).length;
+
+  while (right < s.length) {
+    let char = s[right];
+
+    if (map[char] !== undefined) {
+      map[char]--;
+      if (map[char] === 0) count--;
+    }
+
+    while (count === 0) {
+      let temp = s[left];
+
+      if (map[temp] !== undefined) {
+        map[temp]++;
+        if (map[temp] > 0) count++;
+      }
+
+      if (right - left + 1 < min) {
+        min = right - left + 1;
+        minStr = s.slice(left, right + 1);
+      }
+
+      left++;
+    }
+
+    right++;
+  }
+
+  return minStr;
+}
+
+// refactor above solution
+
+function minWindow(s, t) {
+  let left = 0;
+  let right = 0;
+  let min = Infinity;
+  let minStr = '';
+  let map = {};
+
+  for (let char of t) {
+    map[char] = map[char] + 1 || 1;
+  }
+
+  let count = Object.keys(map).length;
+
+  while (right < s.length) {
+    let char = s[right];
+
+    if (map[char] !== undefined) {
+      map[char]--;
+      if (map[char] === 0) count--;
+    }
+
+    while (count === 0) {
+      let temp = s[left];
+
+      if (map[temp] !== undefined) {
+        map[temp]++;
+        if (map[temp] > 0) count++;
+      }
+
+      if (right - left + 1 < min) {
+        min = right - left + 1;
+        minStr = s.slice(left, right + 1);
+      }
+
+      left++;
+    }
+
+    right++;
+  }
+
+  return minStr;
+}
