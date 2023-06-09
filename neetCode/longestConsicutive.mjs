31ca5e9616ad940e461657882edf08842ac255d7
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2023-06-09 23:54:19 +0530

    another solution

--- neetCode/longestConsicutive.mjs
+++ neetCode/longestConsicutive.mjs
@@ -27,3 +27,28 @@ function longestConsecutive(arr) {
 
 // example 1
 console.log(longestConsecutive([100, 4, 200, 1, 3, 2])); // 4
+
+
+// another solution
+
+function longestConsecutiveTwo(arr) {
+  let longest = 0;
+  let set = new Set(arr);
+
+  for (let num of arr) {
+    if (!set.has(num - 1)) {
+      let currentNum = num;
+      let currentStreak = 1;
+
+      while (set.has(currentNum + 1)) {
+        currentNum++;
+        currentStreak++;
+      }
+
+      longest = Math.max(longest, currentStreak);
+    }
+  }
+
+  return longest; 
+}
+
