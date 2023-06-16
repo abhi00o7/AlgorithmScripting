803ebccabc09b73e61577ffdbd1dfa3b9a0b9817
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2023-06-16 23:00:22 +0530

    add quickSort

--- neetCode/quixkSort.mjs
+++ neetCode/quixkSort.mjs
@@ -16,19 +16,55 @@ While the choice of the pivot value is important, any pivot will do for our purp
 For simplicity, the first or last element could be used.
  */
 
-function quickSort(arr) {
-  let pivot = arr[0];
-  let left = [];
-  let right = [];
-  if (arr.length < 2) {
-    return arr;
-  }
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < pivot) {
-      left.push(arr[i])
-    } else {
-      right.push(arr[i])
+function quickSortRecursive(arr, start, end) {
+  // // Base case or terminating case
+    if (start >= end) {
+        return;
+    }
+    
+    // // Returns pivotIndex
+    let index = partition(arr, start, end);
+    
+    // // Recursively apply the same logic to the left and right subarrays
+    quickSort(arr, start, index - 1);
+    quickSort(arr, index + 1, end);
+}
+
+// example 1
+array = [7, -2, 4, 1, 6, 5, 0, -4, 2];
+quickSortRecursive(array, 0, array.length - 1);
+
+console.log(array);
+
+function quickSortIterative(arr) {
+  // Creating an array that we'll use as a stack, using the push() and pop() functions
+  stack = [];
+
+  // Adding the entire initial array as an "unsorted subarray"
+  stack.push(0);
+  stack.push(arr.length - 1);
+
+  // There isn't an explicit peek() function
+  // The loop repeats as long as we have unsorted subarrays
+  while (stack[stack.length - 1] >= 0) {
+    // Extracting the top unsorted subarray
+    end = stack.pop();
+    start = stack.pop();
+
+    pivotIndex = partition(arr, start, end);
+
+    // If there are unsorted elements to the "left" of the pivot,
+    // we add that subarray to the stack so we can sort it later
+    if (pivotIndex - 1 > start) {
+      stack.push(start);
+      stack.push(pivotIndex - 1);
+    }
+
+    // If there are unsorted elements to the "right" of the pivot,
+    // we add that subarray to the stack so we can sort it later
+    if (pivotIndex + 1 < end) {
+      stack.push(pivotIndex + 1);
+      stack.push(end);
     }
   }
-
-  }
+}
